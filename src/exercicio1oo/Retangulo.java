f2be22db4573c70d93ec738db35c2332d04f25db
Gabe <gabe1946402@gmail.com>
2024-08-26 21:02:26 -0300

    v2

---
+++ src/exercicio1oo/Retangulo.java
@@ -0,0 +1,11 @@
+package exercicio1oo;
+
+public class Retangulo {
+
+    public int base, altura;
+
+    public void imprimirArea() {
+        System.out.println("'Area = " + (base * altura));
+
+    }
+}
